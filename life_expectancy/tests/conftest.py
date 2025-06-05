2d7c53d12138b078dae3197f1abcb8b639cc68a7
diogolamy <diogolamy1999@gmail.com>
2025-06-05 14:56:47 +0100

    add the new fixtures to the conftest.py

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -32,3 +32,13 @@ def run_before_and_after_tests() -> None:
 def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
     return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv")
+
+@pytest.fixture(scope="session")
+def eu_life_expectancy_input() -> pd.DataFrame:
+    """Fixture to load the input sample for testing cleaning."""
+    return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", sep="\t")
+
+@pytest.fixture(scope="session")
+def eu_life_expectancy_expected() -> pd.DataFrame:
+    """Fixture to load the expected cleaned output of the sample."""
+    return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_expected.csv")
