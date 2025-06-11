a2a04c0300b9b0dac6a860abefec265e0a032740
diogolamy <diogolamy1999@gmail.com>
2025-06-11 17:07:22 +0100

    updates in test_save_data.py

--- life_expectancy/tests/test_save_data.py
+++ life_expectancy/tests/test_save_data.py
@@ -11,9 +11,7 @@ def test_save_data_calls_to_csv(eu_life_expectancy_expected, tmp_path):
     when saving to a .csv file. Uses mocking to prevent actual file writing.
     """
     
-    output_path = tmp_path / "test_output.csv"
-
     with patch("pandas.DataFrame.to_csv") as mock_to_csv:
-        save_data(eu_life_expectancy_expected, str(output_path))
+        save_data(eu_life_expectancy_expected, tmp_path / "output.csv")
 
-        mock_to_csv.assert_called_once_with(str(output_path), index=False)
+        mock_to_csv.assert_called_once_with(str(tmp_path / "output.csv"), index=False)
