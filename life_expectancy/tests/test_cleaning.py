a41371cfee26db2a1329da0c3e63f77ba4cecb75
diogolamy <diogolamy1999@gmail.com>
2025-06-11 12:46:08 +0100

    updates on test_cleaning.py

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -8,16 +8,15 @@ from . import FIXTURES_DIR
 def test_main(eu_life_expectancy_expected):
     """Run the 'main' function and compare the output to the expected output"""
 
-    output_path = FIXTURES_DIR / "test_main_output.csv"
-
     args = [
         str(FIXTURES_DIR / "eu_life_expectancy_raw.tsv"),
-        str(output_path),
+        str(FIXTURES_DIR / "eu_life_expectancy_expected.csv"),
         "--country", "PT"
     ]
 
-    main(args)
+    result = main(args)
 
-    actual_df = pd.read_csv(output_path)
-
-    pd.testing.assert_frame_equal(actual_df, eu_life_expectancy_expected)
+    pd.testing.assert_frame_equal(
+    result.reset_index(drop=True),
+    eu_life_expectancy_expected.reset_index(drop=True)
+)
