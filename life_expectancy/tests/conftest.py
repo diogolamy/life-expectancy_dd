c7cf4a88dcc1bb21360747ac59507812a0027395
diogolamy <diogolamy1999@gmail.com>
2025-06-13 12:17:23 +0100

    delete scope=session from the input fixture

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -9,7 +9,7 @@ def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
     return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv")
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def eu_life_expectancy_input() -> pd.DataFrame:
     """Fixture to load the input sample for testing cleaning."""
     return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", sep="\t")
